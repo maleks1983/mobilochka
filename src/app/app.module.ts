8638a42eb13853cd02b972df71a9f2954246624b
MAleksandr <maleks1983@gmail.com>
2023-02-04 16:36:39 +0200

    add header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeadrComponent } from './headr/headr.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadrComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/headr/headr.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-headr',
+  templateUrl: './headr.component.html',
+  styleUrls: ['./headr.component.css']
+})
+export class HeadrComponent {
+
+}
